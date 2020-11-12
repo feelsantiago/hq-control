2dc6efa3acc77b2f3f1806f19a0dfd3090eb3e9f
Filipe Santiago <porto.santiago@hotmail.com>
2020-11-12 20:09:19 -0300

    add modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { CollectionController } from './collection/collection.controller';
+import { ComicController } from './comic/comic.controller';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/hq-control')],
+    imports: [MongooseModule.forRoot('mongodb://localhost/hq-control'), ComicController, CollectionController],
     controllers: [],
     providers: [],
 })
